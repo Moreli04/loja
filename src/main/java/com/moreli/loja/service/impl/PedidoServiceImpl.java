a7d21dfe0df6ad38568f08bb1352fb429f683c7f
Moreli04 <leosmoreli@gmail.com>
2020-11-01 18:19:53 -0300

    Inserindo pedido

--- src/main/java/com/moreli/loja/service/impl/PedidoServiceImpl.java
+++ src/main/java/com/moreli/loja/service/impl/PedidoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.moreli.loja.service.impl;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.moreli.loja.enums.EstadoPagamento;
 import com.moreli.loja.exceptions.ObjectNotFoundException;
-import com.moreli.loja.model.Pagamento;
+import com.moreli.loja.model.ItemPedido;
 import com.moreli.loja.model.PagamentoComBoleto;
 import com.moreli.loja.model.Pedido;
+import com.moreli.loja.repository.ItemPedidoRepository;
+import com.moreli.loja.repository.PagamentoRepository;
 import com.moreli.loja.repository.PedidoRepository;
+import com.moreli.loja.repository.ProdutoRepository;
 import com.moreli.loja.service.PedidoService;
+import com.moreli.loja.service.ProdutoService;
 
 @Service
 public class PedidoServiceImpl implements PedidoService {
@@ -21,13 +26,22 @@ public class PedidoServiceImpl implements PedidoService {
 	private PedidoRepository repository;
 	
 	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
 	private PagamentoComBoletoServiceImpl pagamentoComBoletoService;
 
 	@Override
 	@Transactional(readOnly = true)
 	public Pedido findById(Long id) {
 		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! id: " + id + " tipo: " + Pedido.class.getSimpleName()));
+				"Objeto não encontrado! id: " + id + " titemPedidoo: " + Pedido.class.getSimpleName()));
 	}
 
 	@Override
@@ -41,6 +55,16 @@ public class PedidoServiceImpl implements PedidoService {
 			final PagamentoComBoleto pagamento = (PagamentoComBoleto) pedido.getPagamento();
 			pagamentoComBoletoService.prepararPagamentoParaInclusao(pagamento);
 		}
-		return null;
+		repository.save(pedido);
+		pagamentoRepository.save(pedido.getPagamento());
+		
+		for (ItemPedido itemPedido : pedido.getItens()) {
+			itemPedido.setDesconto(BigDecimal.ZERO);
+			itemPedido.setProduto(produtoService.findById(itemPedido.getProduto().getId()));
+			itemPedido.setPreco(itemPedido.getProduto().getPreco());
+			itemPedido.setPedido(pedido);
+		}
+		itemPedidoRepository.saveAll(pedido.getItens());
+		return pedido;
 	}
 }
